c8d31e7fedac3d29c30860627629eda419184dcf
epasquel <epasquel_20@hotmail.com>
2012-03-19 23:31:23 -0500

    Inicio de Proyecto

---
+++ ProyectoFinalPOO/test/ProyectoFinalPOO/ProyectoFinalPOOTest.java
@@ -0,0 +1,25 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ProyectoFinalPOO;
+
+import org.junit.AfterClass;
+import org.junit.Test;
+import static org.junit.Assert.*;
+import org.junit.BeforeClass;
+
+/**
+ *
+ * @author student
+ */
+public class ProyectoFinalPOOTest {
+    
+    public ProyectoFinalPOOTest() {
+    }
+
+    @Test
+    public void siUsuarioNoExisteDebeMandarMensaje(){
+        
+    }
+}
